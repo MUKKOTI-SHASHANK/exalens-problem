1e4648f632a09693aa6748e07f31d5f4713079fe
MUKKOTI-SHASHANK <119873232+MUKKOTI-SHASHANK@users.noreply.github.com>
2023-01-04 21:51:20 +0530

    Updated Solution without any testcase

--- solution.js
+++ solution.js
@@ -37,34 +37,35 @@ function apartmentHunting(blocks, reqs) {
   ).idx;
 }
 
-console.log(apartmentHunting(blocks = [ 
-  { 
-  "gym": false, 
-  "school": true, 
-  "store": false, 
-  }, 
-  { 
-  "gym": true, 
-  "school": false, 
-  "store": false, 
-  }, 
-  { 
-  "gym": true, 
-  "school": true, 
-  "store": false, 
-  }, 
-  { 
-  "gym": false, 
-  "school": true, 
-  "store": false, 
-  }, 
-  { 
-  "gym": false, 
-  "school": true, 
-  "store": true, 
-  } 
-  ],
-  reqs = ["gym", "school", "store"]
-  ))
+// console.log(apartmentHunting(blocks = [ 
+//   { 
+//   "gym": false, 
+//   "school": true, 
+//   "store": false, 
+//   }, 
+//   { 
+//   "gym": true, 
+//   "school": false, 
+//   "store": false, 
+//   }, 
+//   { 
+//   "gym": true, 
+//   "school": true, 
+//   "store": false, 
+//   }, 
+//   { 
+//   "gym": false, 
+//   "school": true, 
+//   "store": false, 
+//   }, 
+//   { 
+//   "gym": false, 
+//   "school": true, 
+//   "store": true, 
+//   } 
+//   ],
+//   reqs = ["gym", "school", "store"]
+//   ))
+
 // Do not edit the line below.
 exports.apartmentHunting = apartmentHunting;
